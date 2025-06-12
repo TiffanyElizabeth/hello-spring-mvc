922cea8f3932ed4d71a6edfd9f75897968a4e5ea
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-06-12 23:59:29 +0200

    first commit

---
+++ src/main/java/org/lessons/spring/hello_spring_mvc/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package org.lessons.spring.hello_spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import java.time.LocalTime;
+
+@Controller
+@RequestMapping("/")
+public class GreetingController {
+    @GetMapping("/hello")
+    public String greeting(Model model, @RequestParam(name = "name") String name) {
+        model.addAttribute("name", name);
+        model.addAttribute("time", LocalTime.now());
+
+        return "greeting";
+    }
+}
